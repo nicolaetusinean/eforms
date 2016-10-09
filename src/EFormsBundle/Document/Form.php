5430ea636ff39b3913eaad4ef10c3e7c5bc1e4f6
Mihai Stancu <mihai.stancu@emag.ro>
2016-10-09 15:09:57 +0300

    View rendering from DB

--- src/EFormsBundle/Document/Form.php
+++ src/EFormsBundle/Document/Form.php
@@ -2,7 +2,6 @@
 
 namespace EFormsBundle\Document;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use EFormsBundle\Traits\ConstructableProperties;
 
